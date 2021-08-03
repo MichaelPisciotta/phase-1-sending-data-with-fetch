649b49f1d5fcc93c9d73a7c578fe0757abe259b1
MichaelPisciotta <mfp56@miami.edu>
2021-08-02 21:06:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+async function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body:JSON.stringify(name, email)
+    })
+    .then(res => res.json())
+
+
+ }
+
+
+
+
+//  const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle"
+//     })
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject); 
